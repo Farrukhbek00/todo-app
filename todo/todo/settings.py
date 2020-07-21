7383d4b2de3e34c0a79652a7b1797cbad589dd83
Farrukhbek <farruhzokirov00@gmail.com>
2020-07-21 15:33:02 +0500

    visual todo app ready

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todoapp.apps.TodoappConfig',
+    'frontend.apps.FrontendConfig',
     'rest_framework',
 ]
 
